434cc60d357174e95d3db5df38edf29a88c2c957
cracklings3d <39695148+cracklings3d@users.noreply.github.com>
2023-12-13 16:41:17 +0800

    Revert "proper DLL export and import"
    This reverts commit a9245ccce359b7e24739e53efb1b4792a5592647.

---
+++ PCG-Supplemental-Library/PSL_Clustering.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include <vector>
+
+#include "PSL_clustering_impl.h"
+#include "include/PSL_clustering.h"
+#include "PSL_domain.h"
+
+
+std::vector<size_t> PSL::Clustering::detect_cluster(std::vector<double> points) {
+  if (points.size() == 0) return {};
+
+  std::vector<size_t> val; 
+  PSL::Point_set ps; 
+  assert(points.size() % 3 == 0); 
+
+  for (size_t i = 0; i < points.size(); i += 3) {
+    ps.insert(PSL::Point_3(points[i], points[i + 1], points[i + 2]));
+  }
+
+  auto clustered_points = PSL::Clustering_impl::detect_cluster(ps);
+
+  const size_t *d = clustered_points.property_map<size_t>("cluster id").first.data();
+  // copy from d to val
+  std::copy_n(d, points.size(), std::back_inserter(val));
+
+  assert(points.size() == val.size());
+  return val;
+}
